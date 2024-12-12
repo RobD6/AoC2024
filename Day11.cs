c866345db4e6edbc1244fa92a7a59c54eccbe045
Rob <rob_dodd6@hotmail.com>
2024-12-12 07:54:53 +0000

    Day 11 part 2

--- Day11.cs
+++ Day11.cs
@@ -46,17 +46,60 @@ public class Day11
         return newStones;
     }
 
+    private Dictionary<long, long> BlinkSets(Dictionary<long, long> stoneDict)
+    {
+        Dictionary<long, long> newDict = new();
+
+        foreach ((long stone, long count) in stoneDict)
+        {
+            if (stone == 0)
+            {
+                newDict.TryAdd(1, 0);
+                newDict[1] += count;
+                continue;
+            }
+            string s = stone.ToString();
+            if (s.Length % 2 == 0)
+            {
+                int half = s.Length / 2;
+                long firstHalf = long.Parse(s.Substring(0, half));
+                long secondHalf = long.Parse(s.Substring(half));
+
+                newDict.TryAdd(firstHalf, 0);
+                newDict.TryAdd(secondHalf, 0);
+                newDict[firstHalf] += count;
+                newDict[secondHalf] += count;
+                continue;
+            }
+
+            newDict.TryAdd(stone * 2024, 0);
+            newDict[stone * 2024] += count;
+        }
+
+        return newDict;
+    }
+
     public void Part2()
     {
         var data = Utils.LoadData(11).Trim();
 
         List<long> stones = data.Split(' ').Select(long.Parse).ToList();
 
-        for (long i = 0; i < 75; i++)
+        Dictionary<long, long> stoneSets = new();
+
+        foreach (var stone in stones)
         {
-            stones = Blink(ref stones);
+            stoneSets.TryAdd(stone, 0);
+
+            stoneSets[stone]++;
         }
 
-        Console.WriteLine(stones.Count);
+        for (int i = 0; i < 75; i++)
+        {
+            stoneSets = BlinkSets(stoneSets);
+        }
+
+        long sum = stoneSets.Values.Sum();
+        Console.WriteLine(sum);
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 using AoC2024;
 
 var solver = new Day11();
+
 System.DateTime start = System.DateTime.Now;
 solver.Part1();
+System.Console.WriteLine("Completed in " + (System.DateTime.Now - start));
+
+start = System.DateTime.Now;
 solver.Part2();
 System.Console.WriteLine("Completed in " + (System.DateTime.Now - start));
